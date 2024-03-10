0e5cdf0342b367c3ed53998c96bb0d35601f3f84
Luquebas <lucas.fonseca.sesisenaisp@gmail.com>
2024-03-10 13:28:21 -0300

    Configurações de Segurança e Filtro para JWToken

---
+++ src/main/java/com/api_controle_acesso/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.api_controle_acesso.security;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.api_controle_acesso.repositories.UsuarioRepository;
+import com.api_controle_acesso.services.JWTService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        if (tokenJWT != null) {
+            var subject = jwtService.validarToken(tokenJWT);
+            var usuario = usuarioRepository.findByMatricula(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+       }
+
+       filterChain.doFilter(request, response);
+
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null)
+            return authorizationHeader.replace("Bearer", "");
+        
+        return null;
+    }
+    
+}
