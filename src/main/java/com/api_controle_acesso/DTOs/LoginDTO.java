19041308ecf73cc905c539e11306730fb858f160
Luquebas <lucas.fonseca.sesisenaisp@gmail.com>
2024-03-18 20:07:39 -0300

    Changes

--- src/main/java/com/api_controle_acesso/DTOs/LoginDTO.java
+++
@@ -1,6 +0,0 @@
-package com.api_controle_acesso.DTOs;
-import jakarta.validation.constraints.NotBlank;
-
-public record LoginDTO(@NotBlank String matricula, @NotBlank String senha) {
-
-}  
--- src/main/java/com/api_controle_acesso/DTOs/TokenDTO.java
+++
@@ -1,5 +0,0 @@
-package com.api_controle_acesso.DTOs;
-
-public record TokenDTO(String tokenJWT) {
-
-} 
