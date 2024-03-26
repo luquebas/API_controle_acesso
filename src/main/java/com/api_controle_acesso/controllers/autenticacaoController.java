c6e7d55520c9631c56fa43f220f3286ca1aa3a93
Luquebas <lucas.fonseca.sesisenaisp@gmail.com>
2024-03-26 20:37:39 -0300

    Endpoint de Validação do JWToken

--- src/main/java/com/api_controle_acesso/controllers/autenticacaoController.java
+++ src/main/java/com/api_controle_acesso/controllers/autenticacaoController.java
@@ -3,15 +3,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.api_controle_acesso.DTOs.AuthDTO.LoginDTO;
 import com.api_controle_acesso.DTOs.AuthDTO.TokenDTO;
 import com.api_controle_acesso.models.Usuario;
 import com.api_controle_acesso.services.JWTService;
+import com.api_controle_acesso.services.UsuarioService;
+
 import jakarta.validation.Valid;
 
 @RestController
@@ -24,13 +27,28 @@ public class autenticacaoController {
     @Autowired
     private JWTService jwtService;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @PostMapping
     public ResponseEntity<Object> login(@RequestBody @Valid LoginDTO loginDto) {
         
         var authToken = new UsernamePasswordAuthenticationToken(loginDto.matricula(), loginDto.senha());
+        var usuario = usuarioService.findUsuarioByMatricula(loginDto.matricula());
+
         var authentication = authenticationManager.authenticate(authToken);
+
         var jwtToken = jwtService.gerarToken((Usuario) authentication.getPrincipal());
 
-        return ResponseEntity.ok(new TokenDTO(jwtToken));
+        return ResponseEntity.ok(new TokenDTO(usuario.getId(), usuario.getNome(), usuario.getEmail(), usuario.getFoto(), jwtToken));
     }
+
+    @GetMapping("/validarToken")
+    public ResponseEntity<Object> validarToken(@RequestHeader(name = "Authorization", defaultValue = "") String auth) throws Exception {
+
+        jwtService.tokenValido(auth);
+        
+        return ResponseEntity.ok().build();
+    }
+
 }
